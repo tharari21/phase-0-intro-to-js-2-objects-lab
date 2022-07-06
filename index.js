755c70dec3c31e8dea8aaedeba19383c8ae687ca
Tomer Harari <tharari21@gmail.com>
2022-07-05 21:15:13 -0400

    commit tree 45301060cf54c0a61f81cd59a8cfe8d742d12ec9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {
+  name: "Tomer Harari",
+  streetAddress: "2542 East 65th Street",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newObj = { ...employee };
+  delete newObj[key];
+  return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
